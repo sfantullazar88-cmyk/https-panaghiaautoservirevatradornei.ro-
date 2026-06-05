a6a93ae11a3490ba15e9443f98285c7c5d0a1bca
Valica Emanuel <emailul-tau@example.com>
2026-06-05 17:48:40 +0300

    use google maps api key from env

--- frontend/src/pages/admin/AdminDelivery.jsx
+++ frontend/src/pages/admin/AdminDelivery.jsx
@@ -29,7 +29,7 @@ const AdminDelivery = () => {
     } else if (!window.google) {
       // Load Google Maps script
       const script = document.createElement('script');
-      script.src = `https://maps.googleapis.com/maps/api/js?key=&libraries=places`;
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}&libraries=places`;
       script.async = true;
       script.defer = true;
       script.onload = () => initMap();
