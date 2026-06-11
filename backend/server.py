3ae6aed4d485d4db6d04f553cf9732b0000f89ee
Valica Emanuel <emailul-tau@example.com>
2026-06-11 15:45:57 +0300

    fix apirouter import

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, APIRouter
 from dotenv import load_dotenv
 from starlette.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
